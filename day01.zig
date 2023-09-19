91ec928aa5c95b038ccc6c2210df7c5fb003a0a5
littleBobbyTabelz <jonas.st@mailbox.org>
2023-09-19 21:52:05 +0200

    first day part 2

--- day01.zig
+++ day01.zig
@@ -15,15 +15,34 @@ pub fn main() !void {
             weight += item_weight;
         }
         try cumulated.append(weight);
-        std.debug.print("{d}\n----------\n", .{weight});
+        // std.debug.print("{d}\n----------\n", .{weight});
     }
 
     var max: usize = 0;
+    var top_three: [3]usize = [3]usize{ 0, 0, 0 };
     for (cumulated.items) |elv| {
         max = if (elv > max) elv else max;
+        var min: usize = top_three[0];
+        var min_index: usize = 0;
+        for (top_three[1..3], 1..) |enrty, i| {
+            if (enrty < min) {
+                min = enrty;
+                min_index = i;
+            }
+        }
+        if (elv > min) {
+            top_three[min_index] = elv;
+        }
     }
 
-    std.debug.print("Max: {}", .{max});
+    var sum: usize = 0;
+    for (top_three) |item| {
+        sum += item;
+        // std.debug.print("{d}\n----------\n", .{item});
+    }
+
+    std.debug.print("Max: {}\n", .{max});
+    std.debug.print("Sum: {}\n", .{sum});
 }
 
 fn readInput() ![]u8 {
